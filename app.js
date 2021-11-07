cf71cf42c229d05f976271b3c4fc9c7f8d7e14a4
kimoony <hoonby9011@gmail.com>
2021-11-07 23:57:43 +0900

    getting username

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting')
 
-const link = document.querySelector('a');
+const HIDDEN_CLASSNAME = 'hidden';
 
+
+// const link = document.querySelector('a');
 
 function handleLoginSubmit(e) {
   e.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  console.log(username)
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(e) {
-  e.preventDefault();
-  console.log(e)
-  alert('clicked')
-}
+// function handleLinkClick(e) {
+//   e.preventDefault();
+//   console.log(e)
+//   alert('clicked')
+// }
 
 loginForm.addEventListener('submit', handleLoginSubmit);
-link.addEventListener('click', handleLinkClick)
