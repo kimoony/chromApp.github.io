1bda5deb84cb81cdddd4b1dc6d5aeed092dc08e7
kimoony <hoonby9011@gmail.com>
2021-11-11 02:20:36 +0900

    :memo: remark fix
    remark fix

--- JS/weather.js
+++ JS/weather.js
@@ -3,9 +3,11 @@ const city = document.querySelector('#weather span:last-child');
 const API_KEY = '8b4400e8424eb03dac3f2ff4dc6ae6e1'
 
 function onGeoOk(position) {
+  // 위도와 경도를 각가 변수에 담아준다.
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  // fetch를 사용해 얻고자 하는 정보를 받아온다.
   fetch(url)
     .then(res => res.json())
     .then(data => {
